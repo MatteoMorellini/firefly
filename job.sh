1e9d815e871b16d3b169842f71295c734f1fc862
Matteo Morellini <matteomorellini08@gmail.com>
2026-05-19 11:34:17 +0200

    fix: library setup for baldo

---
+++ job.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#SBATCH --job-name=hello_slurm
+#SBATCH --output=my_output_%j.out
+#SBATCH --error=my_error_%j.err
+#SBATCH --partition=edu-short
+#SBATCH --account=gpu.computing26
+#SBATCH --nodes=1
+#SBATCH --gres=gpu:a30.24:1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=1
+
+module purge
+module load CUDA/12.3.2
+module load Python/3.12.3-GCCcore-13.3.0
+
+uv run python main.py
